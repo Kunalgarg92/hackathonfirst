0c9a9331ddb3afb46a4e6f869ba5d74342650312
Kunalgarg92 <125365794+Kunalgarg92@users.noreply.github.com>
2024-03-19 03:07:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+
+function submit1(){
+     var input =document.querySelector(".input1").value;
+     if(input===null){
+        alert("No File selected !");
+     }
+     else{
+        alert("File submitted ");
+     }
+}
+function upload1(){
+    var input1 = document.querySelector(".input2").value;
+    if(input1===null){
+        alert("No File selected!");
+     }
+     else{
+        alert(" File submitted ");
+     }
+}
+function upload2(){
+    var input2 = document.querySelector(".input3").value;
+    if(input2===null){
+        alert("No File selected !");
+     }
+     else{
+        alert("File submitted ");
+     }
+}
